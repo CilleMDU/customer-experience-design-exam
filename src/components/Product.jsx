e860004889896787aa19c4638279806da6636f74
Ranganium <j.korsgaard.kristensen@gmail.com>
2026-03-05 10:11:16 +0100

    udseende af card
    css udseende macher nu Figma, men functioner som visning af mulige farver eller rating fungerer ikke

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,16 +5,16 @@ export default function product({ product }) {
   return (
     <Link to={`/products/${product.id}`} className={styles.cardLink}>
       <article className={styles.card}>
-        <img src={product.image} className={styles.image} />
-        <div className={styles.info}>
-          <h2 className={styles.title}>{product.title}</h2>
-          <p className={styles.price}>DKK {product.price}</p>
-          <span
-            className={`${styles.stock} ${product.inStock ? styles.inStock : styles.outOfStock}`}
-          >
-            {product.inStock ? "In Stock" : "Out of Stock"}
-          </span>
-        </div>
+        <figure className={styles.imageArea}>
+          <img src={product.image} className={styles.image} />
+        </figure>
+        <span className={styles.brandName}>Placeholder</span>
+        <h2 className={styles.title}>{product.title}</h2>
+        <p className={styles.price}>{product.price} kr.</p>
+        <section className={styles.hoverInfo}>
+          <div className={styles.ratingStarsAmount}>stars</div>
+          <div className={styles.colorsAmount}>colors</div>
+        </section>
       </article>
     </Link>
   );
