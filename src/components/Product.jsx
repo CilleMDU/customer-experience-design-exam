7e915952f4ac63a22f12d71f9eaad9877486570c
Ranganium <j.korsgaard.kristensen@gmail.com>
2026-03-05 11:19:36 +0100

    fuldt funktionelt card og grid
    rigtige variabler så ind

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,11 +6,15 @@ export default function product({ product }) {
     <Link to={`/products/${product.id}`} className={styles.cardLink}>
       <article className={styles.card}>
         <figure className={styles.imageArea}>
-          <img src={product.image} className={styles.image} />
+          <img
+            src={product.gallery?.[0]?.image_url}
+            alt={product.gallery?.[0]?.alt_text}
+            className={styles.image}
+          />
         </figure>
-        <span className={styles.brandName}>Placeholder</span>
+        <span className={styles.brandName}>{product.brand}</span>
         <h2 className={styles.title}>{product.title}</h2>
-        <p className={styles.price}>{product.price} kr.</p>
+        <p className={styles.price}>{product.price}</p>
         <section className={styles.hoverInfo}>
           <div className={styles.ratingStarsAmount}>stars</div>
           <div className={styles.colorsAmount}>colors</div>
