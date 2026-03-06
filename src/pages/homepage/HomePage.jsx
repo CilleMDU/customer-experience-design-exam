a5bef45755921d50ee6ef144eb2afe690e82dc57
Frea <fredrikmoellenbergdot@gmail.com>
2026-03-06 13:09:10 +0100

    started-single-item-service-section

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,5 +1,6 @@
 import heroLogo from "../../assets/logo-hero-section.svg";
 import heroBg from "../../assets/hero-section-background.svg";
+import etBen from "../../assets/etben.svg";
 import styles from "./HomePage.module.css";
 
 export default function HomePage() {
@@ -14,21 +15,20 @@ export default function HomePage() {
       </header>
       <main>
         <section>. . . .</section>
-        <section>
-          <h1>Single Item Service</h1>
-          <p>
-            Har du kun brug for et produkt, men de kommer som sæt? Bare rolig,
-            med vores single item service kan du aktivt fravælge den del af et
-            produktsæt du ikke har brug for. Gør det med dine sko, handsker,
-            sokker osv. Vi er her for at hjælpe dig med det du har brug for!
-          </p>
+        <section className={styles.singleItemService}>
+          <div className={styles.serviceText}>
+            <h1 className={styles.singleItemServiceTitle}>
+              Single Item Service
+            </h1>
+            <p className={styles.singleItemServiceText}>
+              Har du kun brug for et produkt, men de kommer som sæt? Bare rolig,
+              med vores single item service kan du aktivt fravælge den del af et
+              produktsæt du ikke har brug for. Gør det med dine sko, handsker,
+              sokker osv. Vi er her for at hjælpe dig med det du har brug for!
+            </p>
+          </div>
+          <img className={styles.etBen} src={etBen} alt="Et Ben" />
         </section>
-        <h2 className={styles.singleItemServiceTitle}>
-          Welcome to the Home Page
-        </h2>
-        <p className={styles.singleItemServicetext}>
-          This is the main landing page of the application
-        </p>
       </main>
     </>
   );
