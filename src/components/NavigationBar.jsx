1de18ed77d90305cddb7294c2b2717599f0c67d7
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-03-06 11:44:02 +0100

    ikoner tilføjet til navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,5 +1,11 @@
 import { NavLink } from "react-router";
 import styles from "./NavigationBar.module.css";
+import shoppingBag from "../assets/shopping-bag.svg";
+import hjerteIkon from "../assets/heart-icon.svg";
+import profilIkon from "../assets/profile-icon.svg";
+import searchIkon from "../assets/search-icon.svg";
+import darkmodeIkon from "../assets/darkmode-icon.svg";
+import navbarLogo from "../assets/navbar-logo.svg";
 
 export default function NavigationBar() {
   return (
@@ -9,6 +15,15 @@ export default function NavigationBar() {
         <NavLink to="/products">Produkter</NavLink>
         <NavLink to="/about">Om os</NavLink>
       </div>
+      <div className={styles.ikoner}>
+        <img src={shoppingBag} alt="Indkøbskurv" />
+        <img src={hjerteIkon} alt="favoritter" />
+        <img src={profilIkon} alt="profil" />
+        <img src={searchIkon} alt="søge funktion" />
+        <img src={darkmodeIkon} alt="darkmode knap" />
+      </div>
+
+      <img src={navbarLogo} alt="logo" />
     </nav>
   );
 }
