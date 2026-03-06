0605d27989fd245e438fc593ecac30f03bc1aca2
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-03-06 11:20:25 +0100

    navbar ikoner tilføjet, første css på navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,10 +1,14 @@
 import { NavLink } from "react-router";
+import styles from "./NavigationBar.module.css";
 
 export default function NavigationBar() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/products">Products</NavLink>
+    <nav className={styles.NavigationBar}>
+      <div className={styles.pages}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/products">Produkter</NavLink>
+        <NavLink to="/about">Om os</NavLink>
+      </div>
     </nav>
   );
 }
