1c5c59f86566fc3d85561272a98a88c2d2b04544
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-03-09 10:53:00 +0100

    about us page remade commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import NavigationBar from "./components/NavigationBar";
 import FooterBar from "./components/FooterBar";
 import HomePage from "./pages/homepage/homepage";
 import ProductPage from "./pages/productPage/productPage";
-import ProductDetailPage from "./pages/productDetailPages/ProductDetailPage";
+import ProductDetailPage from "./pages/productDetailPages/productDetailPage";
 import ErrorPage from "./pages/errorpage/errorpage";
+import About from "./pages/about/about";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/products" element={<ProductPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
         <Route path="*" element={<ErrorPage />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <FooterBar />
     </>
