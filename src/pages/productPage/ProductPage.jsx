c15a593fd6522488034b4dbbdec2a34b9ea01335
Ranganium <j.korsgaard.kristensen@gmail.com>
2026-03-09 13:09:11 +0100

    Knapper og styling
    ingen funktionalitet

--- src/pages/productPage/ProductPage.jsx
+++ src/pages/productPage/ProductPage.jsx
@@ -1,10 +1,13 @@
+import FooterBar from "../../components/FooterBar";
 import ProductGrid from "../../components/productGrid";
+import { ProductManagement } from "../../components/ProductManagement";
+import style from "./ProductPage.module.css";
 
 export default function ProductPage() {
   return (
     <>
-      <header>
-        <h1>Products</h1>
+      <header className={style.header}>
+        <ProductManagement />
       </header>
       <main>
         <ProductGrid />
