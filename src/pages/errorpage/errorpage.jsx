03cdba984794bbccbe541e89fbcc8d9e0b198ae0
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-03-10 10:24:58 +0100

    error siden, justeringer og css

--- src/pages/errorpage/errorpage.jsx
+++ src/pages/errorpage/errorpage.jsx
@@ -1,16 +1,22 @@
 import styles from "./errorpage.module.css";
+import uterus from "../../assets/uterus.svg";
+
 export default function ErrorPage() {
   return (
     <>
-      <header>
-        <h2 className={styles.h2}>404 - Error</h2>
-      </header>
       <main className={styles.main}>
-        <h3 className={styles.h3}>Åh nej... denne side findes ikke</h3>
-        <p className={styles.p}>
-          Klik på logoet, så kommer du tilbage til startsiden igen, fortsat god
-          shopping!
-        </p>
+        <h1 className={styles.h1}>Åh nej... denne side findes ikke</h1>
+        <div className={styles.div}>
+          <p className={styles.p}>
+            Klik på logoet, eller tilbageknappen oppe i venstre hjørne, så
+            kommer du tilbage til startsiden igen, fortsat god shopping!
+          </p>
+          <img
+            className={styles.uterus}
+            src={uterus}
+            alt="billede af maskotten uterus, som er en livmoder med en dumbell i den ene ægestok og som former et hjerte med den anden æggestok"
+          />
+        </div>
       </main>
     </>
   );
