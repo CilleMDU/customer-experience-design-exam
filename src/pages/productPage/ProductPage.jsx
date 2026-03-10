00cedd63c9980049c00d932afda25cb8623687c2
Ranganium <j.korsgaard.kristensen@gmail.com>
2026-03-10 11:26:44 +0100

    filter stuff at the moment
    overlay fungerer, når man klikker, men der er ingen funktionalitet

--- src/pages/productPage/ProductPage.jsx
+++ src/pages/productPage/ProductPage.jsx
@@ -1,14 +1,10 @@
 import FooterBar from "../../components/FooterBar";
-import ProductGrid from "../../components/productGrid";
-import { ProductManagement } from "../../components/ProductManagement";
-import style from "./ProductPage.module.css";
+import ProductGrid from "../../components/ProductGrid";
+// import styles from "./ProductPage.module.css";
 
 export default function ProductPage() {
   return (
     <>
-      <header className={style.ProductPage_header}>
-        <ProductManagement />
-      </header>
       <main>
         <ProductGrid />
       </main>
