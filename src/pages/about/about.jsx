139f8107fd567757df80180c1ea9f3acf9900c85
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-03-10 09:34:10 +0100

    om os siden, justeringer og css

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -4,22 +4,22 @@ export default function About() {
   return (
     <>
       <main className={styles.main}>
-        <div>
-          <h2 className={styles.h2}>Uniquely Her som brand</h2>
+        <section className={styles.tekst}>
+          <h1 className={styles.h1}>Uniquely Her, hvem er vi?</h1>
           <p className={styles.p}>
-            Uniquely Her stræber efter at inkluderer og omfavne kvinder i alle
-            de former og farver de kommer i. Træningstøj skal være noget kvinder
-            kan bruge når de vil svede, gøre rent, slappe af eller som et
-            værktøj der gør hverdagen nemmere at forholde sig til. Vores brand
-            baserer sig på kropspositivitet, friske elementer og at hverdagen er
-            den vi lever størstedelen af vores liv i. Vores site skal både være
-            et sted man kan shoppe, men også et sted man kan føle sig set.
-            Træningstøj er lige så meget til atleter, som til hverdagsheltene
-            der løber hurtigt for at nå karrieren, familielivet eller jagter
-            komfort gennem fleksibelt og åndbart materiale. Vi er et brand lavet
-            til kvinder, som de er
+            Uniquely Her er et brand, der stræber efter at inkludere og omfavne
+            kvinder i alle de former og farver de kommer i. Træningstøj skal
+            være noget kvinderne kan bruge når de vil svede, gøre rent, slappe
+            af eller som et værktøj der gør hverdagen nemmere at forholde sig
+            til. Vores brand baserer sig på kropspositivitet, friske elementer
+            og at hverdagen er den vi lever størstedelen af vores liv i. Vores
+            site skal både være et sted man kan shoppe, men også et sted man kan
+            føle sig set. Træningstøj er lige så meget til atleter, som til
+            hverdagsheltene der løber hurtigt for at nå karrieren, familielivet
+            eller jagter komfort gennem fleksibelt og åndbart materiale. Vi er
+            et brand lavet til kvinder, lige præcis som de er.
           </p>
-        </div>
+        </section>
         <img className={styles.heroLogo} src={heroLogo} alt="logo" />
       </main>
     </>
